5f5768f69c519844411dca89fda20c32cf023317
yonatonreid <yonatonreid@gmail.com>
2021-06-18 14:16:19 -0400

    added new button submit form element

---
+++ src/Laminas/Form/Element/ButtonSubmit.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Bridge\Laminas\Form\Element;
+
+
+class ButtonSubmit extends Button
+{
+    /**
+     * Seed attributes
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'type' => 'submit',
+    ];
+}
