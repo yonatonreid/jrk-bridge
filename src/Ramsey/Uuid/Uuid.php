5fb9835942cb87bddb3abf73e637a1f393621f6c
yonatonreid <yonatonreid@gmail.com>
2021-08-06 23:01:21 -0400

    composer update

--- src/Ramsey/Uuid/Uuid.php
+++ src/Ramsey/Uuid/Uuid.php
@@ -6,5 +6,9 @@ namespace Bridge\Ramsey\Uuid;
 
 class Uuid extends \Ramsey\Uuid\Uuid
 {
-
+    public static function uuid4WithoutSlashes()
+    {
+        $uuid = parent ::uuid4();
+        return str_ireplace('-', '', $uuid);
+    }
 }
