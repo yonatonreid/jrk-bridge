2bad0e728c4b208e44dff66f09525f06d82350ae
yonatonreid <yonatonreid@gmail.com>
2021-09-11 04:16:51 -0400

    aws s3

---
+++ src/Aws/S3/S3Client.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bridge\Aws\S3;
+
+class S3Client extends \Aws\S3\S3Client
+{
+    public function getBucketsSimple():array{
+        $buckets = $this -> listBuckets();
+        $fBuckets = [];
+        foreach ($buckets['Buckets'] as $bucket) {
+            $fBuckets[] = [
+                'name' => $bucket['Name'],
+                'creationDate' => $bucket['CreationDate'] -> __toString()
+            ];
+        }
+        return $fBuckets;
+    }
+}
