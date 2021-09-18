da78c92e33b3ab4019d564d8b4be416383561ecc
yonatonreid <yonatonreid@gmail.com>
2021-09-18 02:22:04 -0400

    aws s3 upload object

--- src/Aws/S3/S3Client.php
+++ src/Aws/S3/S3Client.php
@@ -2,8 +2,20 @@
 
 namespace Bridge\Aws\S3;
 
+use Aws\Result;
+
 class S3Client extends \Aws\S3\S3Client
 {
+    public function getBucketNames(): array
+    {
+        $buckets = $this -> listBuckets();
+        $fBuckets = [];
+        foreach ($buckets['Buckets'] as $bucket) {
+            $fBuckets[] = $bucket['Name'];
+        }
+        return $fBuckets;
+    }
+
     public function getBucketsSimple(): array
     {
         $buckets = $this -> listBuckets();
@@ -20,5 +32,35 @@ class S3Client extends \Aws\S3\S3Client
     public function create(string $bucketName)
     {
         return $this -> createBucket(['Bucket' => $bucketName]);
+    }
+
+    public function uploadFile(string $bucket, string $key, string $filePath, array $metadata = []): Result
+    {
+        $result = $this -> putObject(array(
+            'Bucket' => $bucket,
+            'Key' => $key,
+            'SourceFile' => $filePath,
+            'Metadata' => $metadata
+        ));
+        $this -> waitUntil('ObjectExists', array(
+            'Bucket' => $bucket,
+            'Key' => $key
+        ));
+        return $result;
+    }
+
+    public function createContentFile(string $bucket, string $key, string $data, array $metadata = []): Result
+    {
+        $result = $this -> putObject(array(
+            'Bucket' => $bucket,
+            'Key' => $key,
+            'Body' => $data,
+            'Metadata' => $metadata
+        ));
+        $this -> waitUntil('ObjectExists', array(
+            'Bucket' => $bucket,
+            'Key' => $key
+        ));
+        return $result;
     }
 }
