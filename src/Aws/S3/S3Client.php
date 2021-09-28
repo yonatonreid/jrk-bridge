ffc2d83ee2e8ff27158caf9cf60b32e354662118
yonatonreid <yonatonreid@gmail.com>
2021-09-28 00:58:37 -0400

    s3 delete bucket

--- src/Aws/S3/S3Client.php
+++ src/Aws/S3/S3Client.php
@@ -9,6 +9,15 @@ class S3Client extends \Aws\S3\S3Client
 
     public function deleteBucket(string $bucket): Result
     {
+        $objects = $this->getIterator('ListObjects', ([
+            'Bucket' => $bucket
+        ]));
+        foreach ($objects as $object) {
+            $this->deleteObject([
+                'Bucket' => $bucket,
+                'Key' => $object['Key'],
+            ]);
+        }
         return parent ::deleteBucket([
             'Bucket' => $bucket
         ]);
