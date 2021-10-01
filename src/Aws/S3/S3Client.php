6ddce332207a699f06e9b00ff920001efc9817f6
yonatonreid <yonatonreid@gmail.com>
2021-10-01 04:20:38 -0400

    file size s3

--- src/Aws/S3/S3Client.php
+++ src/Aws/S3/S3Client.php
@@ -39,6 +39,13 @@ class S3Client extends \Aws\S3\S3Client
         ]);
     }
 
+    public function fileSize(string $bucket,string $key): Result
+    {
+        return $this->headObject([
+           'Bucket' => $bucket,
+           'Key' => $key
+        ]);
+    }
     public function getObjects(string $bucket, bool $listAll = false, bool $includeMeta = false): array
     {
         if ($listAll) {
