03259598caacfa8d57c09ab1222cae8af67e0bd8
yonatonreid <yonatonreid@gmail.com>
2021-11-14 00:06:39 -0500

    added laminas bridge

---
+++ src/Laminas/Db/TableGateway/Feature/FeatureSet.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Bridge\Laminas\Db\TableGateway\Feature;
+
+class FeatureSet extends \Laminas\Db\TableGateway\Feature\FeatureSet
+{
+
+}
---
+++ src/Laminas/Filter/Word/CamelCaseToUnderscore.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Bridge\Laminas\Filter\Word;
+
+class CamelCaseToUnderscore extends \Laminas\Filter\Word\CamelCaseToUnderscore
+{
+
+}
